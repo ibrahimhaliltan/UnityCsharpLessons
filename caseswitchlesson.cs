07361e8ec0a918b5868163d8a6f7f4f2f80f3a56
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-07-13 22:46:08 +0300

    Add files via upload

---
+++ caseswitchlesson.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caseswitchlesson : MonoBehaviour
+{
+    
+    void Start()
+    {
+        /*
+        int sayim = 3;
+        */
+
+        string araba = "BMW";
+
+        switch (araba)
+        {
+            case "audi":
+            case "BMW":
+            Debug.Log("Girmiş olduğunuz bir bilgi doğru.");
+            break;
+
+            case "mercedes":
+                Debug.Log("Evet değer Mercedes");
+                break;
+
+            case "Seat":
+                Debug.Log("Evet değer Seat");
+                break;
+
+
+            default:
+                Debug.Log("Uyuşan kayıt bulunamadı.");
+                break;
+
+
+        }
+
+    }
+
+    
+}
---
+++ dowhileexam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dowhileexam : MonoBehaviour
+{
+   
+    void Start()
+    {
+        int sayi = 0;
+
+        do
+        {
+            Debug.Log("Ben do'dan geliyorum.");
+            sayi++;
+
+        } while (sayi <= 1);
+
+        /*
+         * 
+        // break
+        // continue
+        while (sayi < 1)
+        {
+            Debug.Log("Dönüyorum");
+            sayi++;
+        }
+        */
+    }
+
+}
