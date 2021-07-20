090fbc82dbca2d6d9e20f903b5cdf5c941e97dd1
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-07-20 17:37:21 +0300

    Add files via upload

---
+++ temelfonksiyonlar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temelfonksiyonlar : MonoBehaviour
+{
+   /* private void Awake()
+    {
+        Debug.Log("Ben awake ile geldim");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("Ben OnEnable ile geldim"); 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ben Start ile geldim");
+    }*/
+
+    // güncelleme fonksiyonlarının sırası
+   private void FixedUpdate()
+    {
+        Debug.Log("Ben FixedUpdate ile sürekli geliyorum");
+        // 0.2
+    }
+    void Update()
+    {
+        Debug.Log("Ben Update ile sürekli geliyorum");
+        // 1 saniye
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("Ben LateUpdate ile sürekli geliyorum");
+    }
+/*
+    private void OnDisable()
+    {
+        Debug.Log("Ben OnDisable ile geldim");
+    }*/
+}
