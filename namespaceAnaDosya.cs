dee07a8caccac09eb3bc4d9f51ada9641999f75d
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-07-20 16:59:55 +0300

    Add files via upload

---
+++ namespaceAnaDosya.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using dusmanyonetimi;
+using puankontrol;
+
+
+public class namespaceAnaDosya : MonoBehaviour
+{
+
+    dusman dusman = new dusman();
+    PuanKontrolClassim puan = new PuanKontrolClassim();
+  
+    void Start()
+    {
+        dusman.saglik(50);
+        puan.kontrol("deneme");
+       
+    }
+
+   
+}
---
+++ namespaceolusturma.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace dusmanyonetimi
+{
+    public class dusman
+    {
+        public void saglik(int sayi)
+        {
+
+            Debug.Log("1. namespace Sağlık " + sayi);
+        }
+
+
+    }
+       
+
+}
+
+
+namespace puankontrol
+{
+
+    public class PuanKontrolClassim
+    {
+        public void kontrol(string ad)
+        {
+
+            Debug.Log("kontrol " + ad);
+        }
+
+
+    }
+
+
+}
