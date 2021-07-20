318ef9deca9238ef2c1b5611b91d4dcaa9fdc5fd
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-07-20 22:22:11 +0300

    Add files via upload

---
+++ Invokemethod.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invokemethod : MonoBehaviour
+{
+    // Invoke
+
+
+    void Start()
+    {
+        // Invoke("yaziyaz", 3f);      
+        InvokeRepeating("yaziyaz", 3f,.8f);
+    }
+
+   void yaziyaz()
+    {
+        Debug.Log("Ben fonksiyondan geliyorum");
+
+    }
+
+
+    private void Update()
+    {
+
+        if (IsInvoking("yaziyaz"))     
+            Debug.Log("evet");
+        else
+            Debug.Log("hayır");
+
+
+
+        if (Input.GetKeyDown("space"))
+        {
+            CancelInvoke();
+        }
+    }
+
+
+
+
+}
