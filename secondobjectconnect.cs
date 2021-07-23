275c8d3ed106bf1d47e48455640de6f5cb40079f
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-07-24 01:11:06 +0300

    Add files via upload

---
+++ secondobjectconnect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondobjectconnect : MonoBehaviour
+{
+    public string[] cocuklar;
+    public List<string> babalar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        babalar.Add("Ejder");
+        foreach (var item in cocuklar)
+        {
+            Debug.Log(item);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
