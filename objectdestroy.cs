8a34f41e252f21694b073a7742a615ca49af4218
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-07-26 15:58:47 +0300

    Add files via upload

---
+++ objectdestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectdestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy(gameObject,2f);
+        //Destroy(this);
+        Destroy(GetComponent<CapsuleCollider>());
+    }
+
+   
+}
