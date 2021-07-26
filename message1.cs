07f9660392d2296e3633cc63e6604d6718597643
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-07-26 15:13:50 +0300

    Add files via upload

---
+++ message1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class message1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SendMessage("pressscreen","Ejder",SendMessageOptions.DontRequireReceiver);
+        gameObject.BroadcastMessage("presscreen2");
+    }
+
+    void presscreen2()
+    {
+        Debug.Log("i coming to main object");
+    }
+    void pressscreen(string name)
+    {
+        Debug.Log("Hello World xd" + name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ message2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class message2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void presscreen2()
+    {
+        Debug.Log("i coming to main child object");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
