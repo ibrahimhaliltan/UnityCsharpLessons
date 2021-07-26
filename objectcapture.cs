7276faeb9348c2948387a14f7b991c41b00ab6e9
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-07-26 14:25:37 +0300

    Add files via upload

---
+++ objectcapture.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectcapture : MonoBehaviour
+{
+
+    GameObject myObject;
+    GameObject[] multipleobject;
+
+    public GameObject Outsider;
+    void Start()
+    {
+        CapsuleCollider mycapsule = gameObject.GetComponent(typeof(CapsuleCollider)) as CapsuleCollider;
+         
+        mycapsule.isTrigger = true;
+
+        Outsider.GetComponent<objectcontrol>().yas = 60;
+       multipleobject = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var item in multipleobject)
+        {
+            item.GetComponent<objectcontrol>().yas = 3;
+        }
+
+
+
+        myObject = transform.Find("Child").GetComponent<AudioSource>.mute = true;
+        
+        myObject = GameObject.FindWithTag("Monster");
+        myObject.GetComponent<objectcontrol>().yas = 20;
+        
+        
+        myObject = GameObject.Find("Main");
+        myObject.("Main").GetComponent<objectcontrol>().yas = 15;
+        myObject.GetComponent<objectcontrol>().yas = 19;
+
+
+    }
