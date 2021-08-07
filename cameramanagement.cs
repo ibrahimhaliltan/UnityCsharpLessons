6bf39e2b495d50910d3e03e36b364f511edde502
Halil İbrahim TAN <87371416+ibrahimhaliltan@users.noreply.github.com>
2021-08-07 13:46:04 +0300

    Add files via upload

---
+++ cameramanagement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramanagement : MonoBehaviour
+{
+    public Camera mymaincam;
+    public Camera mysecondcam;
+    public Camera mythirdcam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            mymaincam.enabled = false;
+            mythirdcam.enabled = false;
+            mysecondcam.enabled = true;
+            mysecondcam.fieldOfView = 141f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            mymaincam.enabled = false;
+            mythirdcam.enabled = true;
+            mysecondcam.enabled = false;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mymaincam.enabled = true;
+            mythirdcam.enabled = false;
+            mysecondcam.enabled = false;
+        }
+    }
+}
